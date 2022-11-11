32c8a4967f9b1ef10fc075301f2bd23364fdb83f
shafa-commits <francois.0302.student@ifosupwavre.be>
2022-11-11 19:39:11 +0100

    override configure

--- src/main/java/be/ifosup/learning/config/WebSecurityConfig.java
+++ src/main/java/be/ifosup/learning/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -91,5 +92,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .sessionManagement().maximumSessions(1).expiredUrl("/login");
 
+    }
+
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("img/**");
     }
 }
