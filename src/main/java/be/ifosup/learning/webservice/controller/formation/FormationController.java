e3cb683b8b78c78a8bec8bfcc20f4b537129c8da
LydiaInes <74443402+LydiaInes@users.noreply.github.com>
2022-11-14 16:32:44 +0100

    Create.html
    Reste: update + delete

--- src/main/java/be/ifosup/learning/webservice/controller/formation/FormationController.java
+++ src/main/java/be/ifosup/learning/webservice/controller/formation/FormationController.java
@@ -4,8 +4,13 @@ package be.ifosup.learning.webservice.controller.formation;
 import be.ifosup.learning.formations.service.FormationService;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 import be.ifosup.learning.formations.in.CreateFormationIn;
@@ -13,6 +18,9 @@ import be.ifosup.learning.formations.out.FormationOut;
 import org.springframework.http.*;
 
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -27,6 +35,30 @@ public class FormationController {
     public String formationpage(Model model) {
         model.addAttribute("formations", formationservice.listAll());
         return "formation/index";
+    }
+
+    @GetMapping("/create")
+    public String formationcreatepage(Model model) {
+        model.addAttribute("formations", new CreateFormationIn());
+        return "/formation/create.html";
+    }
+
+    @PostMapping("/create")
+    public String createFormation(@Valid @ModelAttribute("formations") CreateFormationIn createFormationIn, Model model) {
+
+        //Informations de la formation
+        String formationTitre = createFormationIn.getTitre();
+        String formationNumeleve = createFormationIn.getNum_eleve().toString();
+
+        try {
+            formationservice.save(createFormationIn);
+        }
+
+        catch(Exception e){
+            return "redirect:/formation/create.html";
+        }
+
+        return "redirect:/formation/";
     }
 
 
