9885cfd049daddbe8ced996675821ff775fe0bf8
shafa-commits <francois.0302.student@ifosupwavre.be>
2022-11-19 18:12:37 +0100

    th fragments et lien js Ok

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,7 @@
+/* IFOSUP Wavre | Cours IPID avec Mr Sebastien LEJEUNE | Année académique: 2022 - 2023 */
+/* Groupe: Learning Project */
+
+
+function showMsgAlert(){
+    alert("Bienvenue sur Learning Project");
+}
