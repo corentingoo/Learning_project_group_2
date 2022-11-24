7609ffe629019b3903fea50a07a88e42fb6f5c69
francoisifo <90777424+francoisifo@users.noreply.github.com>
2022-11-24 20:36:44 +0100

    wip   faeature 15

--- src/main/java/be/ifosup/learning/webservice/controller/admin/UserController.java
+++ src/main/java/be/ifosup/learning/webservice/controller/admin/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +17,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 
-/** Pour indiquer à Spring que nous sommes en API RESTful
- *  avec communication en JSON */
+
 @Controller
 @RequestMapping("/admin/users")
 public class UserController {
@@ -27,49 +27,54 @@ public class UserController {
     private UsersService usersService;
 
     /**
-     * API read
+     *  read
      */
-    @GetMapping("/users")
-    public List<UserOut> list(){
-        return usersService.listAll();
-    }
-
-    /** API read by id */
-    @GetMapping("/users/{id}")
-    /** @PathVariable càd récupère une valeur dans le chemin de la ressource. */
-    public ResponseEntity<User> get(@PathVariable Long id){
-        try{
-            UserOut user = usersService.get(id);
-            return new ResponseEntity<User>((MultiValueMap<String, String>) user, HttpStatus.OK);
-        } catch (NoSuchElementException e){
-            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
-        }
+    @GetMapping()
+    public String list(Model model){
+        model.addAttribute("users",usersService.listAll());
+        return "admin/users/index";
     }
 
 
-    /** API create */
-    @PostMapping("/users")
-    public void add(@RequestBody UserIn user){
-        usersService.save(user);
-    }
-
-    /** API update */
-    @PutMapping("/users/{id}")
-    public ResponseEntity<?> update(@RequestBody UserIn user, @PathVariable Long id){
-        try{
-            UserOut existUser = usersService.get(id);
-            usersService.save(user);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (NoSuchElementException e){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-
-
-    /** API delete */
-    @DeleteMapping("/users/{id}")
-    public void delete(@PathVariable Long id){
-        usersService.delete(id);
-    }
+//
+//    /** read by id */
+//    @GetMapping("/users/{id}")
+//    /** @PathVariable càd récupère une valeur dans le chemin de la ressource. */
+//    public ResponseEntity<User> get(@PathVariable Long id){
+//        try{
+//            UserOut user = usersService.get(id);
+//            return new ResponseEntity<User>((MultiValueMap<String, String>) user, HttpStatus.OK);
+//        } catch (NoSuchElementException e){
+//            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+//
+//
+//    /** create */
+//    @PostMapping("/users")
+//    public void add(@RequestBody UserIn user){
+//        usersService.save(user);
+//    }
+//
+//
+//
+//    /** update */
+//    @PutMapping("/users/{id}")
+//    public ResponseEntity<?> update(@RequestBody UserIn user, @PathVariable Long id){
+//        try{
+//            UserOut existUser = usersService.get(id);
+//            usersService.save(user);
+//            return new ResponseEntity<>(HttpStatus.OK);
+//        } catch (NoSuchElementException e){
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+//
+//
+//
+//    /** delete */
+//    @DeleteMapping("/users/{id}")
+//    public void delete(@PathVariable Long id){
+//        usersService.delete(id);
+//    }
 }
