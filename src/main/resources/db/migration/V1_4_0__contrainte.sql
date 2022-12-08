34ad9e86874b51efc77c3d9e9d9b8fd5be1c6e88
EthanGoratel <julien_iseux@msn.com>
2022-12-08 18:07:06 +0100

    correction suit au pull dev

--- src/main/resources/db/migration/V1_4_0__contrainte.sql
+++ src/main/resources/db/migration/V1_4_0__contrainte.sql
@@ -1,8 +1,6 @@
 
-create index INDEX_FORMATION_USER on users (id_user);
 create index INDEX_INSCRIPTION_USER on users (id_user);
 create index INDEX_INSCRIPTION_FORMATION on formations (formation_id);
 
-alter table formations add constraint FK_form_us foreign key (teacher) references users (id_user);
 alter table inscriptions add constraint FK_insc_form foreign key (student_id) references users (id_user);
 alter table inscriptions add constraint FK_insc_us foreign key (formation_id) references formations (formation_id);
